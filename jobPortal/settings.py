3b1919701e548301244f312d841196e656e1c7bf
Dhamilare <samuelholuwatosin@gmail.com>
2025-12-05 18:29:00 +0100

    new changes

--- jobPortal/settings.py
+++ jobPortal/settings.py
@@ -84,23 +84,13 @@ WSGI_APPLICATION = 'jobPortal.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-# if config('DATABASE_URL', default=None):
-#     # Production: Use Postgres
-#     DATABASES = {
-#         'default': dj_database_url.config(
-#             default=config('DATABASE_URL'),
-#             conn_max_age=600,
-#             ssl_require=True
-#         )
+  
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
-# else:
-#     # Development: Use SQLite
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.sqlite3',
-#             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#         }
-#     }
+# }
 
 
 DATABASES = {
