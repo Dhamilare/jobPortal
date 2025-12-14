d6f287434572acd5f606bc4a789932c9437adff5
Dhamilare <samuelholuwatosin@gmail.com>
2025-12-14 09:05:56 +0100

    new changes

--- jobPortal/settings.py
+++ jobPortal/settings.py
@@ -87,33 +87,33 @@ WSGI_APPLICATION = 'jobPortal.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
   
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
-
 DATABASES = {
-
     'default': {
-
-        'ENGINE': 'django.db.backends.postgresql',
-
-        'NAME': 'postgres',
-
-        'USER': 'postgres',
-
-        'PASSWORD': 'Klassnics@1759',
-
-        'HOST': 'localhost',
-
-        'PORT': '5432',
-
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-
 }
+
+
+# DATABASES = {
+
+#     'default': {
+
+#         'ENGINE': 'django.db.backends.postgresql',
+
+#         'NAME': 'postgres',
+
+#         'USER': 'postgres',
+
+#         'PASSWORD': 'Klassnics@1759',
+
+#         'HOST': 'localhost',
+
+#         'PORT': '5432',
+
+#     }
+
+# }
 
 
 # Password validation
@@ -160,6 +160,9 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
     "staticfiles": {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
